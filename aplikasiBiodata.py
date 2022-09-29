3950e6f211bbea829169d59a9a0eb700ce227cc2
22tiabdulrahman <114466248+22tiabdulrahman@users.noreply.github.com>
2022-09-28 21:23:11 -0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,21 @@
+#aplikasi biodata buatan saya abdul rahman jainun
+
+#input nama
+nama= input("masukan nama anda!")
+# print / output nama
+print("nama anda adalah :",nama)
+
+#input kelas
+kelas= input("masukan kelas anda!")
+# print / output kelas
+print("kelas anda adalah :",kelas)
+
+#input prodi
+prodi= input("masukan prodi anda!")
+# print / output prodi
+print("prodi anda adalah :",prodi)
+
+#input nim
+NIM= input("masukin nim anda!")
+# print / output prodi
+NIM= input("NIM anda adalah :",NIM)

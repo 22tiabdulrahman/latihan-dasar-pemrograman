13b38e93d5ee37c5e5d1a06c7dc5a89fcf439d71
22tiabdulrahman <114466248+22tiabdulrahman@users.noreply.github.com>
2022-10-05 19:11:31 -0700

    Add files via upload

---
+++ latihan sesi03.py
@@ -0,0 +1,7 @@
+
+nama = "abdul rahman jainun"
+kelas = 'a'
+jurusan = 'teknik informatika'
+quote = 'maju terus jangan pernah berhenti'
+
+print ('nama =', nama, '\n', 'kelas =', kelas, '\n' 'jurusan =',jurusan , '\n' 'quote =',quote , '\n' )

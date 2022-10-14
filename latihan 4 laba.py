0bd2d57d4480cfd949a6d682acf000083c3a59ab
22tiabdulrahman <114466248+22tiabdulrahman@users.noreply.github.com>
2022-10-14 10:52:05 +0700

    Add files via upload

---
+++ latihan 4 laba.py
@@ -0,0 +1,66 @@
+# deskriptif
+# membuat varibel nama barang
+# membuat varibel harga barang
+# membuat varibel persen harga
+# input nama barang
+# input harga barang
+# menghitung harga barang
+# harga barang * persen / 100
+# print harga barang beserta nama barang
+
+nama_barang =input("\nMasukan nama barang:")
+harga_barang1 =int(input("\nMasukan harga barang:"))
+nama_barang =input("\nMasukan nama barang:")
+harga_barang2 = int(input("\nMasukan harga barang:"))
+nama_barang = input("\nMasukan nama barang:")
+harga_barang3 = int(input("\nMasukan harga barang:"))
+
+persen = 10
+total=harga_barang1+harga_barang2+harga_barang3
+hargapersen=int(total*10/100)
+print("\n\nHarga barang setelah di persenkan:",hargapersen)
+print("\nTotal harga keuntungan:",total+hargapersen)
+# deskriptif
+# membuat varibel nama barang
+# membuat varibel harga barang
+# membuat varibel persen harga
+# input nama barang
+# input harga barang
+# menghitung harga barang
+# harga barang * persen / 100
+# print harga barang beserta nama barang
+
+nama_barang =input("\nMasukan nama barang:")
+harga_barang1 =int(input("\nMasukan harga barang:"))
+nama_barang =input("\nMasukan nama barang:")
+harga_barang2 = int(input("\nMasukan harga barang:"))
+nama_barang = input("\nMasukan nama barang:")
+harga_barang3 = int(input("\nMasukan harga barang:"))
+
+persen = 10
+total=harga_barang1+harga_barang2+harga_barang3
+hargapersen=int(total*10/100)
+print("\n\nHarga barang setelah di persenkan:",hargapersen)
+print("\nTotal harga keuntungan:",total+hargapersen)
+# deskriptif
+# membuat varibel nama barang
+# membuat varibel harga barang
+# membuat varibel persen harga
+# input nama barang
+# input harga barang
+# menghitung harga barang
+# harga barang * persen / 100
+# print harga barang beserta nama barang
+
+nama_barang =input("\nMasukan nama barang:")
+harga_barang1 =int(input("\nMasukan harga barang:"))
+nama_barang =input("\nMasukan nama barang:")
+harga_barang2 = int(input("\nMasukan harga barang:"))
+nama_barang = input("\nMasukan nama barang:")
+harga_barang3 = int(input("\nMasukan harga barang:"))
+
+persen = 10
+total=harga_barang1+harga_barang2+harga_barang3
+hargapersen=int(total*10/100)
+print("\n\nHarga barang setelah di persenkan:",hargapersen)
+print("\nTotal harga keuntungan:",total+hargapersen)
